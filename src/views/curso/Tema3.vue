ae0cb72ed63f978b535dfd44e0e9137a58e08d74
Emily Dev <emilse.a.b@gmail.com>
2025-04-21 18:30:02 -0500

    se finaliza maquetación

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -42,12 +42,65 @@
         .col-lg-8.order-2.order-lg-1
       
           LineaTiempoD.color-primario.large(data-aos="fade-right")
-            div(numero="1" titulo="Título paso 1")
-              p Contenido paso 1
-            div(numero="2" titulo="Título paso 2")
-              p Contenido paso 2
-            div(numero="3" titulo="Título paso 3")
-              p Contenido paso 3
+            div(numero="1" titulo="Optimización en logística y distribución de productos")
+              p La logística es un área clave donde la programación lineal permite #[strong optimizar el transporte y la distribución de bienes], asegurando #[strong menores costos y mayor eficiencia en las entregas].
+              p #[strong Caso práctico:]
+              p Una empresa de comercio electrónico como Amazon o Mercado Libre, enfrenta el reto de distribuir paquetes desde sus almacenes hasta los clientes en diferentes ciudades. Para ello, necesita determinar la mejor manera de asignar los pedidos a los vehículos de reparto, minimizando costos y tiempos de entrega.
+              p #[strong Método aplicado.] Modelo de transporte con Programación Lineal.
+              ul.lista-ul--color.color-dark.mb-4
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Reducción de costos operativos en la distribución.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Mayor eficiencia en las rutas de transporte.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Disminución de tiempos de entrega y mejora en la satisfacción del cliente (Kong, 2013).
+              
+            div(numero="2" titulo="Planeación de la producción y asignación de recursos")
+              p En la industria manufacturera, la optimización de la producción es esencial para #[strong garantizar un uso eficiente de la materia prima], #[strong minimizar desperdicios] y #[strong cumplir con la demanda del mercado].
+              p #[strong Caso práctico:]
+              p Una fábrica de #[strong automóviles] como #[strong Toyota], necesita decidir cuántos vehículos de cada modelo producir en un mes. Para ello, se deben considerar:
+              ul.lista-ul--color.color-dark.mb-4
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | La capacidad de producción de cada planta.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | La disponibilidad de materia prima y mano de obra.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | La demanda proyectada en el mercado.
+              
+              p #[strong Método aplicado.] Método Simplex para la Optimización de la Producción.
+              p #[strong Beneficios empresariales:]
+              ul.lista-ul--color.color-dark.mb-4
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Reducción del costo de fabricación.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Mejor aprovechamiento de la capacidad instalada.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Ajuste dinámico de la producción según la demanda del mercado (Taha, 2012).
+            div(numero="3" titulo="Gestión de inventarios y control de abastecimiento")
+              p El control de inventarios es una parte fundamental de la cadena de suministro, ya que un mal manejo del stock puede generar #[strong pérdidas económicas] por #[strong exceso de productos] o #[strong desabastecimientos].
+              p #[strong Caso práctico:] 
+              p Un supermercado como #[strong Walmart] debe determinar #[strong cuántas unidades de cada producto almacenar en sus tiendas para garantizar disponibilidad sin generar sobrecostos.]
+              p #[strong Método aplicado.] Modelos de control de inventarios con Programación Lineal.
+              p #[strong Beneficios empresariales:]
+              ul.lista-ul--color.color-dark.mb-4
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Optimización del almacenamiento y reducción del costo de inventarios.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Prevención de quiebres de stock y mejora en la disponibilidad de productos.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Implementación de modelos de reposición eficiente para mantener el stock óptimo (Puente Riofrío & Gavilánez Álvarez, 2018).
           
         .col-lg-4.order-1.order-lg-2
           figure
@@ -63,17 +116,17 @@
       
       h2.mb-4 Fundamentos de la Programación Lineal en la distribución de recursos
 
-      .row.mb-5
+      .row.mb-2
         .col-lg-4
           figure
             img(src="@/assets/curso/tema3/5.png", data-aos="zoom-in")
         .col-lg-8
           .bg-color-3.p-4(data-aos="fade-left").mb-4
-            p.mb-2 WinQSB (Quantitative Systems for Business) es una herramienta de apoyo para la toma de decisiones cuantitativas en el ámbito empresarial. Fue desarrollado para resolver problemas de investigación de operaciones utilizando modelos matemáticos avanzados de optimización.
+            p.mb-4 WinQSB (Quantitative Systems for Business) es una herramienta de apoyo para la toma de decisiones cuantitativas en el ámbito empresarial. Fue desarrollado para resolver problemas de #[strong investigación de operaciones] utilizando modelos matemáticos avanzados de optimización.
 
-            p.mb-2 Características principales:
+            p.mb-1 #[strong Características principales:] 
 
-            ul.lista-ul--color.color-dark.mb-4
+            ul.lista-ul--color.color-dark.mb-5
               li.mb-0
                 i.fas.fa-arrow-circle-right
                 | Interfaz intuitiva que permite ingresar y analizar datos de manera sencilla.
@@ -89,9 +142,131 @@
               li.mb-0
                 i.fas.fa-arrow-circle-right
                 | Análisis de decisiones basado en modelos matemáticos precisos.
-            
 
-            
+      p WinQSB permite encontrar soluciones óptimas en problemas empresariales que involucran múltiples restricciones y variables, facilitando la toma de decisiones estratégicas y operativas (Hillier & Lieberman, 2010).
+
+      p El software WinQSB se aplica en diversas áreas empresariales, entre las que destacan:
+
+      div.row.justify-content-center.align-items-stretch.mb-4
+        .col-lg-3.mb-4(data-aos="zoom-in-up")
+          div.bg-color-white.box-shadow.px-4.py-5.h-100
+            img.mx-auto.d-block.mb-4(
+              src="@/assets/curso/tema3/6.svg"
+              alt=""
+              style="width: 90px"
+            )
+            h4.text-center Primero
+            p.mb-0.text-center Optimización de rutas y distribución en logística.
+      
+        .col-lg-3.mb-4(data-aos="zoom-in-up")
+          div.bg-color-white.box-shadow.px-4.py-5.h-100
+            img.mx-auto.d-block.mb-4(
+              src="@/assets/curso/tema3/7.svg"
+              alt=""
+              style="width: 90px"
+            )
+            h4.text-center Segundo
+            p.mb-0.text-center Gestión eficiente de inventarios y control de abastecimiento.
+      
+        .col-lg-3.mb-4(data-aos="zoom-in-up")
+          div.bg-color-white.box-shadow.px-4.py-5.h-100
+            img.mx-auto.d-block.mb-4(
+              src="@/assets/curso/tema3/8.svg"
+              alt=""
+              style="width: 90px"
+            )
+            h4.text-center Tercero
+            p.mb-0.text-center Planeación y programación de la producción en fábricas.
+      
+        .col-lg-3.mb-4(data-aos="zoom-in-up")
+          div.bg-color-white.box-shadow.px-4.py-5.h-100
+            img.mx-auto.d-block.mb-4(
+              src="@/assets/curso/tema3/9.svg"
+              alt=""
+              style="width: 90px"
+            )
+            h4.text-center Cuarto
+            p.mb-0.text-center ROptimización de costos y asignación de recursos en proyectos empresariales.
+      h2.mb-4 Ejemplo práctico:
+      .row.mb-5
+        .col-lg-8
+          .bg-color-3.p-4(data-aos="fade-left").mb-4
+            p.mb-4 Una empresa de distribución necesita determinar la mejor forma de asignar productos entre sus tres almacenes y cinco tiendas, asegurando la mínima inversión en transporte y el cumplimiento de la demanda.
+
+            p.mb-4 #[strong Método aplicado.] Modelo de transporte con WinQSB.
+            p #[strong Beneficios:]
+            ul.lista-ul--color.color-dark.mb-4
+              li.mb-2
+                i.fas.fa-arrow-circle-right
+                | Reducción de costos logísticos en un 15 %.
+              li.mb-2
+                i.fas.fa-arrow-circle-right
+                | Distribución equitativa de los productos en función de la demanda.
+              li.mb-2
+                i.fas.fa-arrow-circle-right
+                | Optimización de rutas para evitar cuellos de botella en la distribución (Suñé et al., 2016).
+        
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema3/10.png", data-aos="zoom-in")
+
+        
+      h2.mb-4 Implementación práctica: cómo resolver un problema en WinQSB
+
+      p A continuación, se describe el proceso para resolver un problema de optimización con WinQSB:
+
+      .row.mb-5
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema3/11.png", data-aos="zoom-in")
+        .col-lg-8
+              LineaTiempoD.color-primario.large(data-aos="fade-right")
+                div(numero="1" titulo="Paso 1")
+                  p #[strong Ingresar el modelo de Programación Lineal.]
+                  ul.lista-ul--color.color-dark.mb-4
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Definir la función objetivo (maximizar beneficios o minimizar costos).
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Ingresar las restricciones del problema.
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Establecer las variables de decisión.
+                  
+                div(numero="2" titulo="Paso 2")
+                  p #[strong Ejecutar el cálculo y analizar los resultados.]
+                  ul.lista-ul--color.color-dark.mb-4
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | WinQSB aplicará métodos de optimización, como Simplex o transporte.
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Se generará un reporte con la solución óptima y valores de las variables.
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Se podrá visualizar el impacto de cada restricción en el modelo.
+                div(numero="3" titulo="Paso 3")
+                  p #[strong Interpretación y aplicación de resultados.]  
+                  ul.lista-ul--color.color-dark.mb-4
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Analizar los datos generados para tomar decisiones estratégicas.
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Realizar simulaciones para evaluar distintos escenarios.
+                    li.mb-0
+                      i.fas.fa-arrow-circle-right
+                      | Implementar cambios en la operación para optimizar costos y eficiencia.
+
+      .bg-full-width.bg-color-5.mb-5
+        .px-4.px-md-5.py-4
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/icon-1-12.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.text-white.mb-0 #[b Ejemplo empresarial:] 
+              p.text-white Una empresa de manufactura utiliza WinQSB para optimizar su producción y logra reducir un 20 % de desperdicio de materia prima, aumentando la eficiencia en la fabricación de productos (Taha, 2012).
       
 
       
